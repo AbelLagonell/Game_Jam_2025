30f0cc295877fb7e0ab16803817aecffc2707c3d
Abel-Lagonell <alagonell1730@floridapoly.edu>
2025-02-21 21:57:47 -0500

    Refining Scene Changes and Adding Stat Tracker

---
+++ Code/Scripts/Global/scene_manager.gd
@@ -0,0 +1,25 @@
+extends Node
+class_name NSceneManager
+
+
+@export var Scenes : Dictionary= {};
+
+var m_CurrentSenceAlias : String = "";
+
+func get_scenes() -> Array:
+	return Scenes.keys()
+
+func add_scene(scene_alias: StringName, scene_path: StringName) -> void:
+	Scenes[scene_alias] = scene_path;
+
+func remove_scene(scene_alias: String) -> void:
+	Scenes.erase(scene_alias);
+
+func switch_scene(scene_alias: String) -> void:
+	get_tree().change_scene_to_file(Scenes[scene_alias]); 
+
+func restart_scene() -> void:
+	get_tree().reload_current_scene();
+
+func quit_game() -> void:
+	get_tree().quit()
