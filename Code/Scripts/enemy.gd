83b14efbe3bcccd069d792a4ec683300d9618409
DarkenedRaven <ravencatarina@gmail.com>
2025-02-22 19:07:51 -0500

    new

--- Code/Scripts/enemy.gd
+++ Code/Scripts/enemy.gd
@@ -9,12 +9,13 @@ var damage_taken = 0
 var is_being_held = false
 var start_position = global_position
 var target_position = global_position
+var walk = false;
 
 #Individual values
 var health = 50
 var atk = 5
 var speed = 50
-var stage = 2
+var stage = 0
 
 func start():
 	start_position = global_position
@@ -22,6 +23,7 @@ func start():
 
 func _physics_process(delta):
 	updateForm()
+	animate()
 	
 	if(!is_being_held):
 		if(stage == 2):
@@ -41,7 +43,7 @@ func _physics_process(delta):
 
 func updateForm():
 	if(stage != global.stage):
-		#stage = global.stage
+		stage = global.stage
 		pass
 
 func enemy():
@@ -61,27 +63,32 @@ func updateTargetPos():
 	var targetVec = Vector2(randf_range(-32,32),randf_range(-32,32))
 	target_position = start_position + targetVec
 
-func wander():
-	if(position != target_position):
-		position += (target_position - position)/speed
+func animate():
+	if(walk):
 		if(stage == 0):
 			$AnimatedSprite2D.play("0_walk")
 		if(stage == 1):
 			$AnimatedSprite2D.play("1_walk")
-		if (stage == 2):
+		if(stage == 2):
 			$AnimatedSprite2D.play("2_walk")
-			
-		if(target_position - position) < Vector2(0,0):
-			$AnimatedSprite2D.flip_h = true
-		else:
-			$AnimatedSprite2D.flip_h = false
-	else:
+	if(!walk):
 		if(stage == 0):
 			$AnimatedSprite2D.play("0_idle")
 		if(stage == 1):
 			$AnimatedSprite2D.play("1_idle")
-		if (stage == 2):
+		if(stage == 2):
 			$AnimatedSprite2D.play("2_idle")
+
+func wander():
+	if(int(position.x) == int(target_position.x) && int(position.y) == int(target_position.y)):
+		walk = false;
+	else:
+		position += (target_position - position)/speed
+		walk = true;
+		if(target_position - position) < Vector2(0,0):
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
 
 func _on_wander_timer_timeout():
 	updateTargetPos()
@@ -91,6 +98,7 @@ func _on_damage_cooldown_timeout():
 
 func _on_detection_area_body_entered(body):
 	if body.has_method("player"):
+		print("PlayerFOund")
 		player = body
 		player_chase = true
 
