162b762aa582d79d3eb747cd47734ad2c4e917a4
Abel-Lagonell <alagonell1730@floridapoly.edu>
2025-02-23 13:29:09 -0500

    Fixed Day/Night Also fixed the collision for the pen

--- Code/Scripts/penBehavior.gd
+++ Code/Scripts/penBehavior.gd
@@ -1,19 +1,21 @@
 extends Area2D
 
+func _on_body_shape_entered(body_rid:RID, body:Node2D, body_shape_index:int, local_shape_index:int) -> void:
+	if (!body.has_method("enemy")): return
+	match body.enemyType:
+		"Cow": 
+			global.modify_cow(1)
+		"Sheep": 
+			global.modify_sheep(1)
+		"Chicken": 
+			global.modify_chicken(1)
 
-func _on_body_shape_entered(_body_rid:RID, body:Node2D, _body_shape_index:int, _local_shape_index:int) -> void:
-	if body.has_meta("Cow"):
-		global.modify_cow(1);
-	if body.has_meta("Sheep"):
-		global.modify_sheep(1);
-	if body.has_meta("Chicken"):
-		global.modify_chicken(1);
-
-
-func _on_body_shape_exited(_body_rid:RID, body:Node2D, _body_shape_index:int, _local_shape_index:int) -> void:
-	if body.has_meta("Cow"):
-		global.modify_cow(-1);
-	if body.has_meta("Sheep"):
-		global.modify_sheep(-1);
-	if body.has_meta("Chicken"):
-		global.modify_chicken(-1);
+func _on_body_shape_exited(body_rid:RID, body:Node2D, body_shape_index:int, local_shape_index:int) -> void:
+	if (!body.has_method("enemy")): return
+	match body.enemyType: 
+		"Cow": 
+			global.modify_cow(-1)
+		"Sheep": 
+			global.modify_sheep(-1)
+		"Chicken": 
+			global.modify_chicken(-1)
